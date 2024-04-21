5619e9b187d6a54b10f638f3a7ac298a605739ab
Mihai-Nicolae Dulgheru <dulgherumihai19@stud.ase.ro>
2024-04-21 23:56:11 +0300

    Remove unused files and configurations

---
+++ app.config.js
@@ -0,0 +1,41 @@
+export default {
+  name: 'EcoGadget',
+  slug: 'EcoGadget',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  userInterfaceStyle: 'light',
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#ffffff',
+    },
+    package: 'ro.ase.ecogadget',
+    config: {
+      googleMaps: {
+        apiKey: process.env.GOOGLE_MAPS_API_KEY,
+      },
+    },
+  },
+  web: {
+    favicon: './assets/favicon.png',
+  },
+  plugins: [
+    [
+      'expo-location',
+      {
+        locationAlwaysAndWhenInUsePermission:
+          'Allow $(PRODUCT_NAME) to use your location.',
+      },
+    ],
+  ],
+};
