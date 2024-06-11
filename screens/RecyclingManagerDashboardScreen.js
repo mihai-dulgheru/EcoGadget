bd8291194acf5a6ac1829725cb1f5bb22b1e55f6
Mihai-Nicolae Dulgheru <dulgherumihai19@stud.ase.ro>
2024-06-12 02:53:11 +0300

    feat: Update RecyclingManagerDashboardScreen with animated card components

--- screens/RecyclingManagerDashboardScreen.js
+++ screens/RecyclingManagerDashboardScreen.js
@@ -1,37 +1,63 @@
-import { useContext, useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button, Error, Loading } from '../components/UI';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { useCallback, useContext, useEffect, useState } from 'react';
+import {
+  Animated,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { Error, IconButton, Loading } from '../components/UI';
 import RecyclingManagerService from '../services/RecyclingManagerService';
 import { AuthContext } from '../store/AuthContext';
 import theme from '../styles/theme';
 import { useAxiosAuth } from '../utils/Axios';
+
+function DashboardCard({ icon, title, count, onPress, fadeAnim }) {
+  return (
+    <Animated.View style={[styles.card, { opacity: fadeAnim }]}>
+      <TouchableOpacity style={styles.rowSpaceBetween} onPress={onPress}>
+        <View style={styles.statRowContainer}>
+          <Ionicons name={icon} color={theme.colors.textPrimary} size={20} />
+          <Text style={styles.stat}>{title}</Text>
+        </View>
+        <Text style={styles.stat}>{count}</Text>
+      </TouchableOpacity>
+    </Animated.View>
+  );
+}
 
 export default function RecyclingManagerDashboardScreen({ navigation }) {
   const auth = useContext(AuthContext);
   const [stats, setStats] = useState({
     recyclingLocationCount: 0,
     messageCount: 0,
-    latestRecyclingLocations: [],
-    latestMessages: [],
   });
   const [status, setStatus] = useState('loading');
   const AxiosAuth = useAxiosAuth();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setStatus('loading');
-        const statistics =
-          await RecyclingManagerService.getStatistics(AxiosAuth);
-        setStats(statistics);
-        setStatus('success');
-      } catch (error) {
-        console.error('Error fetching statistics', error);
-        setStatus('error');
-        await auth.signOut();
-      }
-    };
+  const fadeAnim = useState(new Animated.Value(0))[0];
 
+  const fetchData = useCallback(async () => {
+    try {
+      setStatus('loading');
+      const statistics = await RecyclingManagerService.getStatistics(AxiosAuth);
+      setStats(statistics);
+      setStatus('success');
+
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: true,
+      }).start();
+    } catch (error) {
+      console.error('Error fetching statistics', error);
+      setStatus('error');
+      await auth.signOut();
+    }
+  }, [AxiosAuth, auth, fadeAnim]);
+
+  useEffect(() => {
     fetchData();
   }, []);
 
@@ -45,38 +71,29 @@ export default function RecyclingManagerDashboardScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Dashboard</Text>
-      <Text style={styles.stat}>
-        Locații de reciclare:
-        {stats.recyclingLocationCount}
-      </Text>
-      <Text style={styles.stat}>
-        Mesaje utilizatori:
-        {stats.messageCount}
-      </Text>
-      <View>
-        <Text style={styles.title}>Ultimele locații de reciclare</Text>
-        {stats.latestRecyclingLocations.map((location) => (
-          <Text key={location._id}>{location.name}</Text>
-        ))}
-      </View>
-      <View>
-        <Text style={styles.title}>Ultimele mesaje utilizatori</Text>
-        {stats.latestMessages.map((message) => (
-          <Text key={message._id}>{message.message}</Text>
-        ))}
-      </View>
-      <View style={styles.actions}>
-        <Button
-          title="Lista locațiilor de reciclare"
-          onPress={() => navigation.navigate('RecyclingLocationList')}
+      <View style={styles.header}>
+        <Text style={styles.title}>Dashboard</Text>
+        <IconButton
+          icon="log-out"
+          color={theme.colors.textPrimary}
+          size={24}
+          onPress={auth.signOut}
         />
-        <Button
-          title="Lista mesajelor utilizatorilor"
-          onPress={() => navigation.navigate('MessageList')}
-        />
-        <Button title="Deconectare" onPress={auth.signOut} />
       </View>
+      <DashboardCard
+        icon="trash-bin"
+        title="Locații de reciclare"
+        count={stats.recyclingLocationCount}
+        onPress={() => navigation.navigate('RecyclingLocationList')}
+        fadeAnim={fadeAnim}
+      />
+      <DashboardCard
+        icon="chatbox-ellipses"
+        title="Mesaje utilizatori"
+        count={stats.messageCount}
+        onPress={() => navigation.navigate('MessageList')}
+        fadeAnim={fadeAnim}
+      />
     </View>
   );
 }
@@ -87,17 +104,35 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: theme.spacing[4],
   },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: theme.spacing[4],
+  },
   title: {
     ...theme.fontSize['2xl'],
     fontWeight: 'bold',
+  },
+  card: {
+    backgroundColor: theme.colors.backgroundSecondary,
+    borderRadius: theme.borderRadius.lg,
+    padding: theme.spacing[4],
     marginBottom: theme.spacing[4],
+    justifyContent: 'center',
+  },
+  rowSpaceBetween: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  statRowContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: theme.spacing[2],
   },
   stat: {
     ...theme.fontSize.lg,
-    marginBottom: theme.spacing[2],
-  },
-  actions: {
-    gap: theme.spacing[2],
-    marginTop: theme.spacing[4],
+    color: theme.colors.textPrimary,
   },
 });
